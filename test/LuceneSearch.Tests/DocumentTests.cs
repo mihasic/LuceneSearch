3f386da5f15b9dd773f02d9f7e53635a314fa99e
Mykhailo Rybnikov <mihasic@gmail.com>
2017-06-29 17:18:53 +0200

    split packages a bit

--- test/LuceneSearch.Tests/DocumentTests.cs
+++ test/LuceneSearch.Tests/DocumentTests.cs
@@ -9,7 +9,7 @@ namespace LuceneSearch.Tests
     using Microsoft.AspNetCore.Builder.Internal;
     using Xunit;
 
-    public class UnitTest1
+    public class DocumentTests
     {
         [Fact]
         public async Task NotFound()
@@ -24,6 +24,21 @@ namespace LuceneSearch.Tests
             {
                 var x = await client.GetAsync("/anything");
                 x.StatusCode.ShouldBe(HttpStatusCode.NotFound);
+            }
+        }
+        [Fact]
+        public async Task Found()
+        {
+            var dataDirectory = System.IO.Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString("N"));
+            System.IO.Directory.CreateDirectory(dataDirectory);
+            using (var client = new HttpClient(new AspNetCoreHttpMessageHandler(
+                TestServer.Program.ConfigureMiddleware(true, dataDirectory)))
+                {
+                    BaseAddress = new Uri("http://localhost/")
+                })
+            {
+                var x = await client.GetAsync("/" + Uri.EscapeDataString("/authors/OL100656A"));
+                x.StatusCode.ShouldBe(HttpStatusCode.OK);
             }
         }
     }
