39fcd15fd93df5f98daf054b97b01720be161b5b
Mykhailo Rybnikov <mihasic@gmail.com>
2017-08-18 12:41:38 +0200

    fixing linux build

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ for f in src/**/*.csproj; do
     (cd `dirname $f`; dotnet restore)
 done
 for f in src/**/*.csproj; do
-    (cd `dirname $f`; dotnet build -f "netstandard1.6")
+    (cd `dirname $f`; dotnet build)
 done
 
 for f in test/**/*.csproj; do
